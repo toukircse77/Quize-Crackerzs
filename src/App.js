c7038d9ae75d5acc37e4a9318034d6beaf771899
toukircse77 <toukircse777@gmail.com>
2022-10-12 00:05:25 +0600

    complete rechart

--- src/App.js
+++ src/App.js
@@ -37,6 +37,9 @@ function App() {
         },
         {
           path:'statistics',
+          loader: async()=>{
+            return fetch('https://openapi.programming-hero.com/api/quiz');
+          },
           element:<Statistics />
         }
       ]
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 const Statistics = () => {
+    const values = useLoaderData().data;
     return (
         <div>
-            <h1>im here baby</h1>
+            <LineChart width={500} height={300} data={values} >
+            <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+            <Line type="monotone" dataKey="name" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+            </LineChart>
         </div>
     );
 };

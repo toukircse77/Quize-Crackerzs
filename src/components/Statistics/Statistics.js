fcf140a5d24d688147df61bffdacacbdab88caca
toukircse77 <toukircse777@gmail.com>
2022-10-12 17:43:11 +0600

    complete responsive

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import {useLoaderData } from 'react-router-dom';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-
+import './Statistics.css'
 const Statistics = () => {
     const values = useLoaderData().data;
     return (
-        <div>
-            <LineChart width={500} height={300} data={values} >
+        <div className='recharts'>
+            <div>
+            <LineChart width={300} height={300} data={values} >
             <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
             <Line type="monotone" dataKey="name" stroke="#8884d8" activeDot={{ r: 8 }} />
             <Line type="monotone" dataKey="total" stroke="#82ca9d" />
             </LineChart>
+            </div>
+            
         </div>
+        
+        
     );
 };
 
